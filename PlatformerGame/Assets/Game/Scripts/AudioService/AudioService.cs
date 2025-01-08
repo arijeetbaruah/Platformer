b2c33006588e122b1d1022074b6eebc07d87cd5a
arijeet <arijeet@tentworks.io>
2025-01-08 21:33:52 +0530

    scene file relocation

--- PlatformerGame/Assets/Game/Scripts/AudioService/AudioService.cs
+++ PlatformerGame/Assets/Game/Scripts/AudioService/AudioService.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using FMOD.Studio;
 using FMODUnity;
 using PG.Service;
 using UnityEngine;
@@ -6,9 +8,15 @@ namespace PG.Audio
 {
     public class AudioService : IService
     {
-        public AudioService()
+        private List<EventInstance> eventInstances = new List<EventInstance>();
+
+        ~AudioService()
         {
-            
+            foreach (var eventInstance in eventInstances)
+            {
+                eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+                eventInstance.release();
+            }
         }
         
         public void Update()
@@ -24,6 +32,14 @@ namespace PG.Audio
         public void PlayOnce(EventReference audioReference, Vector3 worldPosition)
         {
             RuntimeManager.PlayOneShot(audioReference, worldPosition);
+        }
+
+        public EventInstance CreateInstance(EventReference audioReference)
+        {
+            EventInstance instance = RuntimeManager.CreateInstance(audioReference);
+            eventInstances.Add(instance);
+            
+            return instance;
         }
     }
 }
